c57a1b59af87f12aff6451908171f52a54c36874
tvjudge <tvjudge@learnubuntu.org>
2025-06-05 12:05:22 -0400

    added comment
    added comment for logging deletion completion

--- src/main/java/org/crudapp/controller/EmployeeController.java
+++ src/main/java/org/crudapp/controller/EmployeeController.java
@@ -85,6 +85,7 @@ public class EmployeeController {
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
 
         employeeRepository.delete(employee);
+        //added logging confirming deletion
         logger.info("Deleted employee with id: {}", id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
